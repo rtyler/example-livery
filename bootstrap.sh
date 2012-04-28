e0cd5962228d0540bd54166ea1d87733c3c62533
R. Tyler Croy <tyler@monkeypox.org>
2012-04-28 16:46:21 -0700

    Add a basic bootstrap.sh and a simple manifest

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -xe
 
+apt-get update
+
 apt-get install -y ruby1.8 \
                    ruby1.8-dev \
                    libopenssl-ruby1.8 \
                    rubygems
 
 gem install puppet --no-ri --no-rdoc
+
+export PATH=/var/lib/gems/1.8/bin:$PATH
+
+puppet apply --verbose --modulepath=./modules manifests/site.pp
